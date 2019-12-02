6ce8f58b85f2dbd708560195733756fc89a99a74
SonNguyen2000 <58257235+SonNguyen2000@users.noreply.github.com>
2019-12-02 11:06:24 +0700

    Update IndexController.php

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\table;
+use Auth;
+use App\cart;
+use Session;
 
 class IndexController extends Controller
 {
@@ -13,7 +17,16 @@ class IndexController extends Controller
 
     public function GetIndex()
     {
-    	return view('container.home');
+		
+	    $line_table_dell = table::where('hang','DELL')->paginate(5);
+	    $line_table_asus = table::where('hang','ASUS')->paginate(5);
+	    $line_table_acer = table::where('hang','ACER')->paginate(5);
+	    $line_table_lenovo = table::where('hang','LENOVO')->paginate(5);
+	    $line_table_hp = table::where('hang','HP')->paginate(5);
+	    $line_table_msi = table::where('hang','MSI')->paginate(5);
+
+	    return view('container.home',compact('line_table_dell','line_table_asus','line_table_acer','line_table_lenovo','line_table_hp','line_table_msi'));
+	    	 
     }
 
     public function GetSanPham()
