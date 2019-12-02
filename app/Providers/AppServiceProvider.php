75262de75d54bc00cbac9aff8e2b7b74cd4c0d6c
SonNguyen2000 <58257235+SonNguyen2000@users.noreply.github.com>
2019-12-03 03:38:31 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('container',function($view)
+        {
+            $loai_sp = table::all();
+
+            $view->with('loai_sp',$loai_sp);
+        });
+
+        view()->composer(['container','container.ShoppingCart'],function($view)
+        {
+            if(Session('table'))
+            {
+                $oldCart = Session::get('shoppingcart');
+                $cart = new shoppingcart('$oldCart');
+                $view->with(['cart'=>Session::get('cart'),'laptop_cart'=>$cart->items,'tongtien'=>$cart->tongtien,'sluong'=>$cart->sluong]);
+            }
+        });
     }
 }
