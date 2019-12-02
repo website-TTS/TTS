bebdc31602eea0a1db4e934bb8cf600fa69327cc
SonNguyen2000 <58257235+SonNguyen2000@users.noreply.github.com>
2019-12-03 03:56:27 +0700

    Add files via upload

---
+++ resources/views/container/laptop_cauhinh.blade.php
@@ -0,0 +1,12 @@
+@extends('index')
+@section('css')
+
+	<link rel="stylesheet" href="../common/css/style.css" >
+	<link rel="stylesheet" href="../common/css/StyleCauhinh.css" >
+	<link rel="stylesheet" href="../common/css/bootstrap.min.css">
+	<script src="../common/js/bootstrap.min.js"></script>
+
+@endsection
+@section('container')
+	
+@endsection
